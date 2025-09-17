00dfba25315d268441acc67bf0bcac68ea4b893a
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-09-17 11:11:32 +0200

    feat: Get query parameters

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import a module
 const http = require('http')
 
+const url = require('url')
+
 const content = '<!DOCTYPE html>' +
 '<html>' +
 '    <head>' +
@@ -12,12 +14,25 @@ const content = '<!DOCTYPE html>' +
 '    </body>' +
 '</html>'
 
+const qs = require('querystring')
+
 const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(content)
-  res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(content)
+    res.end()
+    
+    // Retrieve and print the queryParams 
+    const queryParams = qs.parse(url.parse(req.url).query)
+    console.log(queryParams)
+    
+    // Retrieve and print the current path
+    const path = url.parse(req.url).pathname
+    console.log(path)
 }
 
 http
 .createServer(serverHandle)
 .listen(5000)
+
+
+
