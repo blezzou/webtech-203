6cd900f5b3813d76e5658fa4561471fd7240fa2a
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-09-17 14:50:38 +0200

    feat: Get the current path

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Import a module
 const http = require('http')
-
 const url = require('url')
 
 const content = '<!DOCTYPE html>' +
@@ -14,25 +13,17 @@ const content = '<!DOCTYPE html>' +
 '    </body>' +
 '</html>'
 
-const qs = require('querystring')
-
 const serverHandle = function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(content)
-    res.end()
-    
-    // Retrieve and print the queryParams 
-    const queryParams = qs.parse(url.parse(req.url).query)
-    console.log(queryParams)
-    
-    // Retrieve and print the current path
-    const path = url.parse(req.url).pathname
-    console.log(path)
+
+  const path = url.parse(req.url).pathname
+  console.log(path)
+
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write(content)
+  res.write(path)
+  res.end()
 }
 
 http
 .createServer(serverHandle)
 .listen(5000)
-
-
-
