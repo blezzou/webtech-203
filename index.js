cbf59af964d9fd5dce007b2cc77a6955586a2844
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-09-17 15:04:05 +0200

    feat: Organize the source code in a module

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 // Import a module
 const http = require('http')
-const url = require('url')
-const qs = require('querystring')
-
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  const queryParams = qs.parse(url.parse(req.url).query)
-  console.log(queryParams)
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  res.end()
-}
+const handles = require('./handles')
 
 http
-.createServer(serverHandle)
+.createServer(handles.serverHandle)
 .listen(5000)
