5e0551b1011edf5cad369bb98ed2a203788d60f7
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-09-17 14:57:49 +0200

    feat: Basic routing example

--- index.js
+++ index.js
@@ -15,16 +15,20 @@ const content = '<!DOCTYPE html>' +
 '</html>'
 
 const serverHandle = function (req, res) {
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
 
+
+  res.writeHead(200, {'Content-Type': 'text/plain'})
   const queryParams = qs.parse(url.parse(req.url).query)
   console.log(queryParams)
 
-  const path = url.parse(req.url).pathname
-  console.log(queryParams)
-
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(content)
-  res.write(path)
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
   res.end()
 }
 
