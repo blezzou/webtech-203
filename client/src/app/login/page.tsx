5909675a5bdeaecb8883a77cac0a8c50166621a2
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-10-12 21:28:21 +0200

    feat: page se connecter et s'inscrire

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1>Se connecter ou S'inscrire</h1>;
+}
---
+++ client/src/app/run/[id]/register/page.tsx
@@ -0,0 +1,3 @@
+export default function Register() {
+  return <h1>S'inscire à une course</h1>;
+}
