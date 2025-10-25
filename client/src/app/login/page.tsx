182a340af2f0d16c795f3f69e8faa1ef214a9143
Nicolas Pellerin <nicolaspln949@gmail.com>
2025-10-25 15:17:59 +0200

    feat: page connexion

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Login() {
   return (
@@ -14,8 +14,10 @@ export default function Login() {
             className="w-full sm:w-96 bg-[#1a1a1a] border border-neutral-700 rounded-full px-6 py-3 text-lg text-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-md transition"
           />
         </div>
-        <div>        <h2 className="text-5xl sm:text-6xl font-extrabold mb-8 text-white tracking-tight">
-        </h2></div>
+        <div>
+          {" "}
+          <h2 className="text-5xl sm:text-6xl font-extrabold mb-8 text-white tracking-tight"></h2>
+        </div>
         <div className="flex justify-center">
           <input
             type="text"
@@ -23,14 +25,16 @@ export default function Login() {
             className="w-full sm:w-96 bg-[#1a1a1a] border border-neutral-700 rounded-full px-6 py-3 text-lg text-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-md transition"
           />
         </div>
-                <div>        <h2 className="text-5xl sm:text-6xl font-extrabold mb-8 text-white tracking-tight">
-        </h2></div>
-                  <Link
-            href="/login"
-            className="px-5 py-2.5 font-semibold bg-blue-600 text-white rounded-full hover:bg-blue-700 hover:shadow-[0_0_15px_#2563eb80] transition-all duration-300"
-          >
-            Se connecter
-          </Link>
+        <div>
+          {" "}
+          <h2 className="text-5xl sm:text-6xl font-extrabold mb-8 text-white tracking-tight"></h2>
+        </div>
+        <Link
+          href="/login"
+          className="px-5 py-2.5 font-semibold bg-blue-600 text-white rounded-full hover:bg-blue-700 hover:shadow-[0_0_15px_#2563eb80] transition-all duration-300"
+        >
+          Se connecter
+        </Link>
       </section>
     </div>
   );
